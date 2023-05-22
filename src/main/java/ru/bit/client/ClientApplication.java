e02d721b3801f27dce86ce2ba396080c98c04c7a
Galiandina <108342736+feyerinaGO@users.noreply.github.com>
2023-05-22 20:30:45 +0300

    task 3 zookeeper

--- src/main/java/ru/bit/client/ClientApplication.java
+++ src/main/java/ru/bit/client/ClientApplication.java
@@ -4,10 +4,12 @@ package ru.bit.client;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import ru.bit.client.service.ServiceExchange;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientApplication {
     public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext ctx = new SpringApplicationBuilder(ClientApplication.class)
